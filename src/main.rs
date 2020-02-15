3c63bf626f1143e66f56fe77308525c37134e117
blakehawkins <blake.hawkins.11@gmail.com>
2020-02-15 08:08:54 +0000

    improve error-handling

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
-use std::io::Result;
+use std::io::{Result, Write};
 
+use oops::Oops;
 use select::document::Document;
 use select::predicate::Class;
 use stdinix::stdinix;
 use ureq;
 
-
 fn main() -> Result<()> {
     stdinix(|buf| {
-        let body = ureq::get(&buf[..])
-        .call()
-        .into_string()?;
+        let body = ureq::get(&buf[..]).call().into_string()?;
 
-        Document::from(&body[..]).find(Class("propertyCard-link")).for_each(|node| {
-            node.attr("href").filter(|s| s.len() > 0).map(|s| println!("https://www.rightmove.co.uk{}", s));
-        });
+        Document::from(&body[..])
+            .find(Class("propertyCard-link"))
+            .map(|node| {
+                node.attr("href")
+                    .filter(|s| !s.is_empty())
+                    .map(|s| {
+                        println!("https://www.rightmove.co.uk{}", s);
+                        std::io::stdout().flush()
+                    })
+                    .oops(&format!("href missing for {:?}", buf))
+            })
+            .collect::<Result<Vec<_>>>()?;
 
         Ok(())
     })
